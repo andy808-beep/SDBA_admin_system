df8122b1bec80c35eb441078bd23e6986562001f
Andy Wang <andywang@Andys-MacBook-Pro-2.local>
2025-09-11 19:07:58 +0800

    started admin page migration

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabaseClient.ts
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
